e406a97d5a7d3cffeaf2a0f60d2669d681110dfb
younus-always <younusofficial443@gmail.com>
2025-09-10 14:52:38 +0600

    gradient-generator project initialized

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-200">
+        <div className="w-9/12 mx-auto">
+          <div>
+            <h1 className="text-4xl font-bold text-center">🎨 Gradient Generator App</h1>
+            <div>
+              <input type="text" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
